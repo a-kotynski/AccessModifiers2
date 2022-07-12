843d7dd1f3e75723794725ecc36f05240c3e295d
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-12 12:21:07 +0200

    Add project files.

---
+++ AccessModifiers2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2484606#content
+
+// Access Modifiers:
+
+// Public       - accessible from everywhere
+// Private      - accessible only from the class
+// Protected    - best to avoid - accessible only from the class and its derived classes (inheritance?)
+// Internal     - accessible only from the same assembly (assembly = class library)
+// Protected Internal - accessible only from the same assembly or any derived classes
+
+namespace AccessModifiers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
